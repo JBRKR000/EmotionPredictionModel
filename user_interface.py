8b01c4be17ec95245592f87dfa7c967e514c5ba8
JBRKR <kozimorjakub@gmail.com>
2024-04-21 17:56:38 +0200

    - Added progress bar - Added descriptions above functions for easier understanding

--- user_interface.py
+++ user_interface.py
@@ -1,7 +1,6 @@
-from PyQt5.QtCore import pyqtSignal, Qt
-from PyQt5.QtWidgets import QWidget, QVBoxLayout, QTextEdit, QPushButton, QLabel, QComboBox, QMessageBox, QProgressDialog
+from PyQt5.QtCore import pyqtSignal, QThread
+from PyQt5.QtWidgets import QWidget, QVBoxLayout, QTextEdit, QPushButton, QLabel, QComboBox, QMessageBox, QProgressBar
 from model import linear_SVC, logistic_regression_classifier, naive_bayes, predict_emotion
-from threading import Thread
 
 emotion_mapping = {
     0: 'Sadness',
@@ -12,23 +11,17 @@ emotion_mapping = {
     5: 'Surprise'
 }
 
-class ModelLearningThread(Thread):
+class ModelLearningThread(QThread):
+    progress_changed = pyqtSignal(int)
     def __init__(self, window, model_func):
         super().__init__()
         self.window = window
         self.model_func = model_func
 
     def run(self):
+        self.progress_changed.emit(50)
         self.window.model, self.window.vectorizer = self.model_func()
-        self.window.learning_finished.emit()
-
-class LearningFinishedThread(Thread):
-    def __init__(self, window):
-        super().__init__()
-        self.window = window
-
-    def run(self):
-        self.window.display_learning_finished()
+        self.progress_changed.emit(100)
 
 class UserInterface(QWidget):
     learning_finished = pyqtSignal()
@@ -73,40 +66,62 @@ class UserInterface(QWidget):
         self.emotion_display.setReadOnly(True)
         layout.addWidget(self.emotion_display)
 
+        self.progress_bar = QProgressBar()
+        self.progress_bar.setRange(0, 100)
+        layout.addWidget(self.progress_bar)
+
         self.setLayout(layout)
 
         self.predict_button.setEnabled(False)
         self.text_edit.setEnabled(False)
 
     def learn_model(self):
-        selected_model = self.model_combobox.currentText()
-        if selected_model == "linear_SVC":
-            model_func = linear_SVC
-        elif selected_model == "logistic_regression_classifier":
-            model_func = logistic_regression_classifier
-        elif selected_model == "naive_bayes":
-            model_func = naive_bayes
-        else:
-            return
+        try:
+            selected_model = self.model_combobox.currentText()
+            if selected_model == "linear_SVC":
+                model_func = linear_SVC
+            elif selected_model == "logistic_regression_classifier":
+                model_func = logistic_regression_classifier
+            elif selected_model == "naive_bayes":
+                model_func = naive_bayes
+            else:
+                return
 
-        self.learn_button.setEnabled(False)
-        self.model_combobox.setEnabled(False)
+            # Disable buttons and text box while learning
+            self.predict_button.setEnabled(False)
+            self.text_edit.setEnabled(False)
+            self.learn_button.setEnabled(False)
+            self.model_combobox.setEnabled(False)
 
-        self.progress_dialog = QProgressDialog("Learning Model...", None, 0, 100)
-        self.progress_dialog.setWindowModality(Qt.WindowModal)
-        self.progress_dialog.setMinimumDuration(0)
-        self.learning_thread = ModelLearningThread(self, model_func)
-        self.learning_thread.start()
+            # Reset progress bar to 0
+            self.progress_bar.setValue(0)
 
+            # Start the learning thread
+            self.learning_thread = ModelLearningThread(self, model_func)
+            self.learning_thread.progress_changed.connect(self.update_progress)
+            self.learning_thread.finished.connect(self.finish_learning)
+            self.learning_thread.start()
+
+        except Exception as e:
+            QMessageBox.critical(self, "Error", f"An error occurred: {str(e)}")
+
+    # Update learning progress bar
+    def update_progress(self, value):
+        self.progress_bar.setValue(value)
+
+    # Emit finish signal
+    def finish_learning(self):
+        self.learning_finished.emit()
+
+    # Display information about successful learn process
     def display_learning_finished(self):
-        self.progress_dialog.close()
-
         QMessageBox.information(self, "Model Learning", "Model learning finished successfully.")
         self.learn_button.setEnabled(True)
         self.model_combobox.setEnabled(True)
         self.predict_button.setEnabled(True)
         self.text_edit.setEnabled(True)
 
+    # Predict user input with chosen model
     def predict(self):
         if self.model is None:
             return
